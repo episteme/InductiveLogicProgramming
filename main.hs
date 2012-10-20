19eebb1940ec4761a08116f97d61ea3db0ceeda4
Marcus Peacock <marcus@ruination.org.uk>
2012-10-20 19:18:58 +0100

    added simple comments

--- main.hs
+++ main.hs
@@ -34,31 +34,40 @@ nEx = [("daughter", ["tom", "ann"]),
 
 rel = (("daughter", [1, 2]), [("female", [1]), ("parent", [2, 1])])
 
+-- Eventually will return true if relation covers all positive examples
+-- and no negative examples.
+-- Currently ignores negative examples
 covers :: Relation -> BackKnow -> Examples -> Bool
 covers _ _ ([], _) = True
 covers r b ((e:es), x) | allIn b (arg2cla r e) = covers r b (es, x)
                        | otherwise = False
 
+-- allIn x y returns True if y is a subset of x
 allIn :: [Clause] -> [Clause] -> Bool
 allIn b [] = True
 allIn b (x:xs) | x `elem` b = allIn b xs
                | otherwise = False
 
+-- Returns a mapping of indices to literals               
 vMap :: Relation -> Clause -> VarMap
 vMap r c = zip (snd (fst rel)) (snd c)
 
+-- Finds the literal for a given index in a varmap
 getLit :: VarMap -> Integer -> Literal
 getLit (v:vs) i | (fst v) == i = snd v
                 | otherwise = getLit vs i
 
+-- Transforms a 'conjunction' of arg clauses to real clauses
 arg2cla :: Relation -> Clause -> [Clause]
 arg2cla r c = arg2cla_ (snd r) c [] where
                 arg2cla_ [] c s = s
                 arg2cla_ (r2:rs) c s = arg2cla_ rs c ((useVMap (vMap r c) r2):s)
 
+-- Utility function for arg2cla
 useVMap :: VarMap -> ArgClause -> Clause
 useVMap v a = ((fst a), (useVMap2 v (snd a)))
 
+-- Utility function for arg2cla
 useVMap2 :: VarMap -> [Integer] -> [Literal]
 useVMap2 v i = useVMap_ v i [] where
                 useVMap_ v [] s = (reverse s)
