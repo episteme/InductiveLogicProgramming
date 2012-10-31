22713c788a0cb2ffc7c304d24c7a526b823dea01
Marcus Peacock <marcus@ruination.org.uk>
2012-10-31 19:50:02 +0000

    added functions to deal with unbound variables

--- main.hs
+++ main.hs
@@ -1,3 +1,6 @@
+import Debug.Trace
+import Data.List
+
 type Literal = [Char]
 
 type Clause = ([Char], [Literal])
@@ -66,6 +69,34 @@ rel = (("daughter", [1, 2]), [("female", [1]), ("parent", [2, 1])])
 rel2 = (("daugther", [1, 2]), [("female", [1])])
 rel3 = (("cousin", [1, 2]), [("parent", [1,3]), ("sibling", [3,4]), ("parent", [4,2])])
 
+getInVars :: Relation -> [Integer]
+getInVars r = nub $ snd $ fst r
+
+getOutVars :: Relation -> [Integer]
+getOutVars r = nub $ concat $ map snd (snd r)
+
+p2 :: BackKnow -> Integer -> VarMap -> [VarMap]
+p2 b i vm = p3 [(i,y) | y <- (listLits b)] vm [] where
+            p3 :: [(Integer, Literal)] -> VarMap -> [VarMap] -> [VarMap]
+            p3 [] v s = s
+            p3 (x:xs) v s = p3 xs v ((v ++ [x]):s)
+
+-- Idea is to take a list of unbound variables in the form of integers and return
+-- all the possible mappings of these integers to literals
+p4 :: BackKnow -> [Integer] -> [VarMap] -> [VarMap]
+p4 b [] y = y
+p4 b (x:xs) y = p4 b xs (foldr (++) [] (map (p2 b x) y))
+
+possMappings :: BackKnow -> Relation -> Clause -> [VarMap]
+possMappings b r c | diff == [] = vm
+                   | otherwise = p4 b diff vm
+                   where diff = (getOutVars r) \\ (getInVars r)
+                         vm = [zip (getInVars r) (snd c)]
+
+covers2 :: Relation -> BackKnow -> Examples -> Bool
+covers2 _ _ ([], _) = True
+covers2 r b 
+
 -- Returns true if relation covers all positive examples and none of the negative
 -- examples
 covers :: Relation -> BackKnow -> Examples -> Bool
@@ -87,6 +118,7 @@ vMap r c = zip (snd (fst rel)) (snd c)
 
 -- Finds the literal for a given index in a varmap
 getLit :: VarMap -> Integer -> Literal
+getLit [] i | trace ("getLit " ++ (show i)) False = undefined
 getLit (v:vs) i | (fst v) == i = snd v
                 | otherwise = getLit vs i
 
@@ -113,9 +145,8 @@ listLits b = listLits2 b [] where
               listLits2 [] s = s
               listLits2 (x:xs) s = listLits2 xs (addSet (snd x) s)
 
+-- Utility function for listLits
 addSet :: (Eq a) => [a] -> [a] -> [a]
 addSet [] y = y
 addSet (x:xs) y | x `elem` y = addSet xs y
                 | otherwise = addSet xs (x:y)
-
-
