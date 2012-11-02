0021e5b3a42d2dcd5e48f7c0fbbb29719b2ade26
marcus <marcus@air>
2012-11-02 18:51:04 +0000

    concepts of nots, completing knowledge base and full covers func

--- main.hs
+++ main.hs
@@ -3,11 +3,11 @@ import Data.List
 
 type Literal = [Char]
 
-type Clause = ([Char], [Literal])
+type Clause = (Bool, [Char], [Literal])
 
 type Arg = Integer
 
-type ArgClause = ([Char], [Arg])
+type ArgClause = (Bool, [Char], [Arg])
 
 type Relation = (ArgClause, [ArgClause])
 
@@ -21,60 +21,115 @@ type BackKnow = [Clause]
 
 type VarMap = [(Integer, Literal)]
 
-backno = [("parent", ["ann", "mary"]), 
-            ("parent", ["ann", "tom"]),
-            ("parent", ["tom", "eve"]),
-            ("parent", ["tom", "ian"]),
-            ("female", ["ann"]),
-            ("female", ["mary"]),
-            ("female", ["eve"])]
+backno = [(True, "parent", ["ann", "mary"]), 
+            (True, "parent", ["ann", "tom"]),
+            (True, "parent", ["tom", "eve"]),
+            (True, "parent", ["tom", "ian"]),
+            (True, "female", ["ann"]),
+            (True, "female", ["mary"]),
+            (True, "female", ["eve"])]
 
-backno2 = [("sibling", ["james", "steve"]),
-        ("sibling", ["steve", "james"]),
-        ("sibling", ["greg", "jess"]),
-        ("sibling", ["jess", "greg"]),
-        ("sibling", ["bob", "mike"]),
-        ("sibling", ["mike", "bob"]),
-        ("parent", ["bob", "james"]),
-        ("parent", ["bob", "steve"]),
-        ("parent", ["jane", "james"]),
-        ("parent", ["jane", "steve"]),
-        ("parent", ["mike", "greg"]),
-        ("parent", ["mike", "jess"]),
-        ("parent", ["sharon", "greg"]),
-        ("parent", ["sharon", "jess"])]
+backno2 = [(True, "sibling", ["james", "steve"]),
+        (True, "sibling", ["steve", "james"]),
+        (True, "sibling", ["greg", "jess"]),
+        (True, "sibling", ["jess", "greg"]),
+        (True, "sibling", ["bob", "mike"]),
+        (True, "sibling", ["mike", "bob"]),
+        (True, "parent", ["bob", "james"]),
+        (True, "parent", ["bob", "steve"]),
+        (True, "parent", ["jane", "james"]),
+        (True, "parent", ["jane", "steve"]),
+        (True, "parent", ["mike", "greg"]),
+        (True, "parent", ["mike", "jess"]),
+        (True, "parent", ["sharon", "greg"]),
+        (True, "parent", ["sharon", "jess"])]
 
-pEx2 = [("cousin", ["james", "greg"]),
-     ("cousin", ["greg", "james"]),
-     ("cousin", ["steve", "greg"]),
-     ("cousin", ["greg", "steve"]),
-     ("cousin", ["jess", "james"]),
-     ("cousin", ["jess", "steve"]),
-     ("cousin", ["james", "jess"]),
-     ("cousin", ["steve", "jess"])]
+backno4 = [(True, "a", ["1"]),
+        (True, "a", ["2"]),
+        (True, "a", ["3"]),
+        (True, "b", ["4"]),
+        (True, "b", ["5"]),
+        (True, "b", ["6"]),
+        (True, "c", ["7"]),
+        (True, "c", ["8"]),
+        (True, "c", ["9"])]
 
-nEx2 = [("cousin", ["jane", "bob"]),
-     ("cousin", ["greg", "jess"]),
-     ("cousin", ["mike", "sharon"]),
-     ("cousin", ["sharon", "jane"]),
-     ("cousin", ["jane", "jess"])]
+pEx4 = [(True, "d", ["1"]),
+        (True, "d", ["2"]),
+        (True, "d", ["3"]),
+        (True, "d", ["7"]),
+        (True, "d", ["8"]),
+        (True, "d", ["9"])]
 
-pEx = [("daughter", ["mary", "ann"]),
-       ("daughter", ["eve", "tom"])]
+nEx4 = [(True, "d", ["4"]),
+        (True, "d", ["5"]),
+        (True, "d", ["6"])]
 
-nEx = [("daughter", ["tom", "ann"]),
-       ("daughter", ["eve", "ann"])]
+rel0 = ((True, "d", [1]), [(False, "a", [1]), (False, "c", [1])])
+rel01 = ((True, "d", [1]), [(False, "b", [1])])
+rel02 = ((True, "d", [1]), [(True, "a", [1]), (True, "c", [1])])
 
-rel = (("daughter", [1, 2]), [("female", [1]), ("parent", [2, 1])])
-rel2 = (("daugther", [1, 2]), [("female", [1])])
-rel3 = (("cousin", [1, 2]), [("parent", [1,3]), ("sibling", [3,4]), ("parent", [4,2])])
+pEx2 = [(True, "cousin", ["james", "greg"]),
+     (True, "cousin", ["greg", "james"]),
+     (True, "cousin", ["steve", "greg"]),
+     (True, "cousin", ["greg", "steve"]),
+     (True, "cousin", ["jess", "james"]),
+     (True, "cousin", ["jess", "steve"]),
+     (True, "cousin", ["james", "jess"]),
+     (True, "cousin", ["steve", "jess"])]
 
+nEx3 = [(True, "cousin", ["james", "james"]),
+        (True, "cousin", ["sharon", "mike"]),
+        (True, "coisin", ["steve", "james"])]
+
+-- cousin (james, greg) <- parent(bob, james), sibling(bob, mike), parent(mike,
+        -- greg)
+--
+-- 1: james
+-- 2: greg
+-- 3: bob
+-- 4: mike
+
+nEx2 = [(True, "cousin", ["jane", "bob"]),
+     (True, "cousin", ["greg", "jess"]),
+     (True, "cousin", ["mike", "sharon"]),
+     (True, "cousin", ["sharon", "jane"]),
+     (True, "cousin", ["jane", "jess"])]
+
+pEx = [(True, "daughter", ["mary", "ann"]),
+       (True, "daughter", ["eve", "tom"])]
+
+nEx = [(True, "daughter", ["tom", "ann"]),
+       (True, "daughter", ["eve", "ann"])]
+
+rel = ((True, "daughter", [1, 2]), [(True, "female", [1]), (True, "parent", [2, 1])])
+rel2 = ((True, "daugther", [1, 2]), [(True, "female", [1])])
+rel3 = ((True, "cousin", [1, 2]), [(True, "parent", [3, 1]), (True, "sibling", [3,4]),
+        (True, "parent", [4, 2])])
+rel4 = ((True, "cousin", [1, 2]), [(True, "parent", [3, 1])])
+rel5 = ((True, "cousin", [1, 2]), [(True, "sibling", [1, 2])])
+rel6 = ((True, "cousin", [1,2]), [])
+rel7 = ((True, "daughter", [1,2]), [])
+
+-- hoogle?
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, x) = x
+
+-- get the set of ints used on the left of the relation
 getInVars :: Relation -> [Integer]
-getInVars r = nub $ snd $ fst r
+getInVars r = nub $ thd3 $ fst r
 
+-- get the set of ints used on the right of the relation
 getOutVars :: Relation -> [Integer]
-getOutVars r = nub $ concat $ map snd (snd r)
+getOutVars r = nub $ concat $ map thd3 (snd r)
 
+-- utility for p4, messy: needs restructure and rename
 p2 :: BackKnow -> Integer -> VarMap -> [VarMap]
 p2 b i vm = p3 [(i,y) | y <- (listLits b)] vm [] where
             p3 :: [(Integer, Literal)] -> VarMap -> [VarMap] -> [VarMap]
@@ -83,54 +138,51 @@ p2 b i vm = p3 [(i,y) | y <- (listLits b)] vm [] where
 
 -- Idea is to take a list of unbound variables in the form of integers and return
 -- all the possible mappings of these integers to literals
+-- cant believe i figured this out, needs restructure and rename
 p4 :: BackKnow -> [Integer] -> [VarMap] -> [VarMap]
 p4 b [] y = y
 p4 b (x:xs) y = p4 b xs (foldr (++) [] (map (p2 b x) y))
+
+-- does this relation cover the positive examples?
+coverspos :: Relation -> BackKnow -> [Clause] -> Bool
+coverspos r b [] = True
+coverspos r b (c:cs) | tryVMaps (possMappings b r c) r b c == False = False
+                     | otherwise = coverspos r b cs
+
+-- does this relation cover no negative examples?
+coversnoneg :: Relation -> BackKnow -> [Clause] -> Bool
+coversnoneg r b [] = True
+coversnoneg r b (c:cs) | tryVMaps (possMappings b r c) r b c == True = False
+                     | otherwise = coversnoneg r b cs
+
+covers :: Relation -> BackKnow -> Examples -> Bool
+covers r b (p, n) = and [(coverspos r b p), (coversnoneg r b n)]
 
 possMappings :: BackKnow -> Relation -> Clause -> [VarMap]
 possMappings b r c | diff == [] = vm
                    | otherwise = p4 b diff vm
                    where diff = (getOutVars r) \\ (getInVars r)
-                         vm = [zip (getInVars r) (snd c)]
+                         vm = [zip (getInVars r) (thd3 c)]
 
-covers2 :: Relation -> BackKnow -> Examples -> Bool
-covers2 _ _ ([], _) = True
-covers2 r b 
+-- do any of these vmappings work?
+tryVMaps :: [VarMap] -> Relation -> BackKnow -> Clause -> Bool
+tryVMaps [] r b c = False
+tryVMaps (x:xs) r b c | tryVMap x r b c == True = True
+                      | otherwise = tryVMaps xs r b c
 
--- Returns true if relation covers all positive examples and none of the negative
--- examples
-covers :: Relation -> BackKnow -> Examples -> Bool
-covers _ _ ([], _) = True
-covers r b (x, (n:ns)) | allIn b (arg2cla r n) == False = covers r b (x, ns)
-                       | otherwise = False
-covers r b ((e:es), []) | allIn b (arg2cla r e) = covers r b (es, [])
-                        | otherwise = False
-
--- allIn x y returns True if y is a subset of x
-allIn :: (Eq a) => [a] -> [a] -> Bool
-allIn b [] = True
-allIn b (x:xs) | x `elem` b = allIn b xs
-               | otherwise = False
-
--- Returns a mapping of indices to literals               
-vMap :: Relation -> Clause -> VarMap
-vMap r c = zip (snd (fst rel)) (snd c)
-
--- Finds the literal for a given index in a varmap
-getLit :: VarMap -> Integer -> Literal
-getLit [] i | trace ("getLit " ++ (show i)) False = undefined
-getLit (v:vs) i | (fst v) == i = snd v
-                | otherwise = getLit vs i
+-- does this mapping fit?
+tryVMap :: VarMap -> Relation -> BackKnow -> Clause -> Bool
+tryVMap v r b c = allIn b (arg2cla v r c)
 
 -- Transforms a 'conjunction' of arg clauses to real clauses
-arg2cla :: Relation -> Clause -> [Clause]
-arg2cla r c = arg2cla_ (snd r) c [] where
+arg2cla :: VarMap -> Relation -> Clause -> [Clause]
+arg2cla v r c = arg2cla_ (snd r) c [] where
                 arg2cla_ [] c s = s
-                arg2cla_ (r2:rs) c s = arg2cla_ rs c ((useVMap (vMap r c) r2):s)
+                arg2cla_ (r2:rs) c s = arg2cla_ rs c ((useVMap v r2):s)
 
 -- Utility function for arg2cla
 useVMap :: VarMap -> ArgClause -> Clause
-useVMap v a = ((fst a), (useVMap2 v (snd a)))
+useVMap v a = ((fst3 a), (snd3 a), (useVMap2 v (thd3 a)))
 
 -- Utility function for arg2cla
 useVMap2 :: VarMap -> [Integer] -> [Literal]
@@ -138,15 +190,77 @@ useVMap2 v i = useVMap_ v i [] where
                 useVMap_ v [] s = (reverse s)
                 useVMap_ v (i:ix) s = useVMap_ v ix ((getLit v i):s)
 
+
+-- hoogle this
+-- allIn x y returns True if y is a subset of x
+allIn :: (Eq a) => [a] -> [a] -> Bool
+allIn b [] = True
+allIn b (x:xs) | x `elem` b = allIn b xs
+               | otherwise = False
+
+-- Finds the literal for a given index in a varmap
+getLit :: VarMap -> Integer -> Literal
+getLit [] i | trace ("getLit " ++ (show i)) False = undefined
+getLit (v:vs) i | (fst v) == i = snd v
+                | otherwise = getLit vs i
+
 -- Returns the list of literals used in the background knowledge
 listLits :: BackKnow -> [Literal]
 listLits [] = []
 listLits b = listLits2 b [] where
               listLits2 [] s = s
-              listLits2 (x:xs) s = listLits2 xs (addSet (snd x) s)
+              listLits2 (x:xs) s = listLits2 xs (addSet (thd3 x) s)
 
+-- hoogle this
 -- Utility function for listLits
 addSet :: (Eq a) => [a] -> [a] -> [a]
 addSet [] y = y
 addSet (x:xs) y | x `elem` y = addSet xs y
                 | otherwise = addSet xs (x:y)
+
+backInfo :: BackKnow -> [([Char], Integer)]
+backInfo [] = []
+backInfo (x:xs) = ((snd3 x), (fromIntegral (length (thd3 x)))):(backInfo (dropWhile (\a -> snd3 a == snd3 x) xs))
+
+allpos :: Integer -> BackKnow -> [[Literal]]
+allpos 1 b = [[x] | x <- listLits b]
+allpos n b = [x ++ [y] | x <- (allpos (n-1) b), y <- (listLits b)]
+
+allpos2 :: BackKnow -> BackKnow
+allpos2 b = ap2 (backInfo b) [] where
+             ap2 [] x = x
+             ap2 (x:xs) y = [(True, (fst x), p) | p <- (allpos (snd x) b)] ++ (ap2 xs y)
+
+showb :: BackKnow -> [Char]
+showb [] = []
+showb (x:xs) = (show (fst3 x)) ++ " " ++ (snd3 x) ++ (show (thd3 x)) ++ "\n" ++ (showb xs)
+
+switchc :: Clause -> Clause
+switchc (True, c, l) = (False, c, l)
+switchc (False, c, l) = (True, c, l)
+
+switchallc :: [Clause] -> [Clause]
+switchallc [] = []
+switchallc (x:xs) = (switchc x):(switchallc xs)
+
+completeb :: BackKnow -> BackKnow
+completeb b = (switchallc ((allpos2 b) \\ b)) ++ b
+
+-- need to generate every useful argclause
+useargs :: Relation -> BackKnow -> [ArgClause]
+useargs r b = mkargcls (backInfo b) (fromIntegral (length (nub ((getOutVars r) ++ getInVars r)))  + 1)
+
+numProd :: Integer -> Integer -> [[Integer]]
+numProd i 1 = [[x] | x <- [1..i]]
+numProd i n = [x ++ [y] | x <- (numProd i (n-1)), y <- [1..i]]
+
+mkargcls :: [([Char], Integer)] -> Integer -> [ArgClause]
+mkargcls [] i = []
+mkargcls (x:xs) i = (mkargcl x i) ++ (mkargcls xs i)
+
+mkargcl :: ([Char], Integer) -> Integer -> [ArgClause]
+mkargcl (c, i) j = [(combt True x) | x <- endl] ++ [(combt False x) | x <- endl] where
+                    endl = zip (replicate (length (numProd j i)) c) (numProd j i)
+
+combt :: a -> (b, c) -> (a, b, c)
+combt x (y, z) = (x, y, z)
